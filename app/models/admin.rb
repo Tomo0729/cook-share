1900e12367e10010ac41838d29f077de394c00d2
Tomoka Murai <you@example.com>
2023-01-23 23:41:33 +0000

    admin登録変更

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,6 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthablee
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
